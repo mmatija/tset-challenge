86964410c3b06b9f9f989056d82059cb66ccefc0
Matija Mocilac <mocilac.matija@gmail.com>
2026-02-27 11:54:42 +0100

    Create project template

---
+++ src/main/kotlin/org/example/tsetchallenge/TsetChallengeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.tsetchallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TsetChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TsetChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/tsetchallenge/TsetChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.tsetchallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TsetChallengeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
