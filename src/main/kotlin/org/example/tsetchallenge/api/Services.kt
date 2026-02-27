9c8b254a4d691407d36e77d3cd4ebc3580239722
Matija Mocilac <mocilac.matija@gmail.com>
2026-02-27 14:18:41 +0100

    Implement request param validation

--- src/main/kotlin/org/example/tsetchallenge/api/Services.kt
+++ src/main/kotlin/org/example/tsetchallenge/api/Services.kt
@@ -1,17 +1,52 @@
 package org.example.tsetchallenge.api
 
+import jakarta.validation.constraints.Positive
 import org.example.tsetchallenge.models.Service
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MissingServletRequestParameterException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.method.annotation.HandlerMethodValidationException
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 @RestController
 @RequestMapping("/services")
 class Services {
 
     @RequestMapping(value = [""], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getServices(): List<Service> {
+    fun getServices(@RequestParam @Positive systemVersion: Int): List<Service> {
         return listOf()
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException::class)
+    fun handleMissingRequestParameterException(): ResponseEntity<Map<String, String>> {
+        val response = buildErrorResponse("systemVersion parameter is missing")
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException::class)
+    fun handleNonPositiveParameterException(): ResponseEntity<Map<String, String>> {
+        val response = buildTypeMismatchError()
+        return ResponseEntity(response, HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun handleNonIntegerParameterException(): ResponseEntity<Map<String, String>> {
+        val response = buildTypeMismatchError()
+        return ResponseEntity(response, HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+
+    private fun buildTypeMismatchError(): HashMap<String, String> =
+        buildErrorResponse("systemVersion parameter must be a positive integer")
+
+    private fun buildErrorResponse(errorMessage: String): HashMap<String, String> {
+        val response = HashMap<String, String>()
+        response["message"] = errorMessage
+        return response
     }
 }
--- src/test/kotlin/org/example/tsetchallenge/ReleaseManagerApiTest.kt
+++ src/test/kotlin/org/example/tsetchallenge/ReleaseManagerApiTest.kt
@@ -15,4 +15,32 @@ class ReleaseManagerApiTest(@Autowired val mockMvc: MockMvc) {
     fun `GET services returns status code 200`() {
         mockMvc.get("/services?systemVersion=1").andExpect { status { isOk() } }
     }
+
+    @Test
+    fun `GET services returns status code 400 when system version is not specified`() {
+        mockMvc.get("/services").andExpect { status { isBadRequest() } }
+    }
+
+    @Test
+    fun `GET services returns error message when system version is not specified`() {
+        val expectedErrorMessage = """{"message": "systemVersion parameter is missing"}"""
+        mockMvc.get("/services").andExpect { content { json(expectedErrorMessage) } }
+    }
+
+    @Test
+    fun `GET services returns status code 422 when systemVersion is not a positive integer`() {
+        mockMvc.get("/services?systemVersion=0").andExpect { status { isUnprocessableContent() } }
+    }
+
+    @Test
+    fun `GET services returns error message when systemVersion is not positive`() {
+        val expectedErrorMessage = """{"message": "systemVersion parameter must be a positive integer"}"""
+        mockMvc.get("/services?systemVersion=0").andExpect { content { json(expectedErrorMessage) } }
+    }
+
+    @Test
+    fun `GET services returns error message when systemVersion is not an integer`() {
+        val expectedErrorMessage = """{"message": "systemVersion parameter must be a positive integer"}"""
+        mockMvc.get("/services?systemVersion=1.2").andExpect { content { json(expectedErrorMessage) } }
+    }
 }
