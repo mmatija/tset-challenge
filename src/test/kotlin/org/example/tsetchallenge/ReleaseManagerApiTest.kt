12ccd2d80be647523316c68f7775a32870cdfe56
Matija Mocilac <mocilac.matija@gmail.com>
2026-02-28 16:34:06 +0100

    Group tests in a nested class

--- src/test/kotlin/org/example/tsetchallenge/ReleaseManagerApiTest.kt
+++ src/test/kotlin/org/example/tsetchallenge/ReleaseManagerApiTest.kt
@@ -1,5 +1,7 @@
 package org.example.tsetchallenge
 
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,36 +13,40 @@ import org.springframework.test.web.servlet.get
 @AutoConfigureMockMvc
 class ReleaseManagerApiTest(@Autowired val mockMvc: MockMvc) {
 
-    @Test
-    fun `GET services returns status code 200`() {
-        mockMvc.get("/services?systemVersion=1").andExpect { status { isOk() } }
-    }
+    @Nested
+    @DisplayName("GET /services")
+    inner class GetReleases {
+        @Test
+        fun `returns status code 200`() {
+            mockMvc.get("/services?systemVersion=1").andExpect { status { isOk() } }
+        }
 
-    @Test
-    fun `GET services returns status code 400 when system version is not specified`() {
-        mockMvc.get("/services").andExpect { status { isBadRequest() } }
-    }
+        @Test
+        fun `returns status code 400 when system version is not specified`() {
+            mockMvc.get("/services").andExpect { status { isBadRequest() } }
+        }
 
-    @Test
-    fun `GET services returns error message when system version is not specified`() {
-        val expectedErrorMessage = """{"message": "systemVersion parameter is missing"}"""
-        mockMvc.get("/services").andExpect { content { json(expectedErrorMessage) } }
-    }
+        @Test
+        fun `returns error message when system version is not specified`() {
+            val expectedErrorMessage = """{"message": "systemVersion parameter is missing"}"""
+            mockMvc.get("/services").andExpect { content { json(expectedErrorMessage) } }
+        }
 
-    @Test
-    fun `GET services returns status code 422 when systemVersion is not a positive integer`() {
-        mockMvc.get("/services?systemVersion=0").andExpect { status { isUnprocessableContent() } }
-    }
+        @Test
+        fun `returns status code 422 when systemVersion is not a positive integer`() {
+            mockMvc.get("/services?systemVersion=0").andExpect { status { isUnprocessableContent() } }
+        }
 
-    @Test
-    fun `GET services returns error message when systemVersion is not positive`() {
-        val expectedErrorMessage = """{"message": "systemVersion parameter must be a positive integer"}"""
-        mockMvc.get("/services?systemVersion=0").andExpect { content { json(expectedErrorMessage) } }
-    }
+        @Test
+        fun `returns error message when systemVersion is not positive`() {
+            val expectedErrorMessage = """{"message": "systemVersion parameter must be a positive integer"}"""
+            mockMvc.get("/services?systemVersion=0").andExpect { content { json(expectedErrorMessage) } }
+        }
 
-    @Test
-    fun `GET services returns error message when systemVersion is not an integer`() {
-        val expectedErrorMessage = """{"message": "systemVersion parameter must be a positive integer"}"""
-        mockMvc.get("/services?systemVersion=1.2").andExpect { content { json(expectedErrorMessage) } }
+        @Test
+        fun `returns error message when systemVersion is not an integer`() {
+            val expectedErrorMessage = """{"message": "systemVersion parameter must be a positive integer"}"""
+            mockMvc.get("/services?systemVersion=1.2").andExpect { content { json(expectedErrorMessage) } }
+        }
     }
 }
